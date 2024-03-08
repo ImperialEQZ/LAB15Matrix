e589ae1b87c234e5d5a7bc6e4aae1bcc65e77fa7
Александр <alexanders.borchenko@gmail.com>
2024-03-08 18:47:49 +0300

    Исправлен 4 пункт, сделан 5-6

--- matrix.c
+++ matrix.c
@@ -1,4 +1,25 @@
 #include "matrix.h"
+#include "array.h"
+
+void swap(void *a, void *b) {
+    void *tmp = a;
+    a = b;
+    b = tmp;
+}
+
+size_t getMaxElementIndexInArray(const int a[], size_t n) {
+
+    int maxIndex = 0;
+
+    for(size_t i = 1; i < n; i++){
+        if(a[i] > a[maxIndex]){
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
 //размещает в динамической памяти матрицу размером nRows на nCols. Возвращает матрицу.
 matrix getMemMatrix(int nRows, int nCols) {
     int **values = (int **) malloc(sizeof(int*) * nRows);
@@ -66,3 +87,94 @@ void swapColumns(matrix m, int j1, int j2) {
         memcpy(&m.values[i] [j2], &j1matrix, sizeof(int*));
     }
 }
+/* выполняет сортировку вставками строк
+матрицы m по неубыванию значения функции criteria применяемой для
+строк. */
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) {
+    int *values = malloc(sizeof(int) * m.nRows);
+    for (size_t i = 0; i < m.nRows; i++)
+        values[i] = criteria(m.values[i], m.nCols);
+
+    for (size_t i = 1; i < m.nRows; i++) {
+        int key = values[i];
+        int j = i - 1;
+        while (key < values[j] && j >= 0) {
+            values[j + 1] = values[j];
+            swapRows(m, j + 1, j);
+            --j;
+        }
+
+        values[j + 1] = key;
+    }
+}
+/*выполняет сортировку выбором столбцов
+матрицы m по неубыванию значения функции criteria применяемой для столбцов
+*/
+void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)) {
+    int *values = malloc(sizeof(int) * m.nCols);
+    for ( size_t i = 0; i < m.nCols; i++) {
+        int *col = malloc( sizeof(int) * m.nRows);
+        for (size_t j = 0; j < m.nRows; j++)
+            col[j] = m.values[j][i];
+
+        values[i] = criteria(col, m.nRows);
+    }
+
+    for (int i = m.nCols - 1; i > 0; --i) {
+        int max = getMaxElementIndexInArray(values, i + 1);
+        if (max != i) {
+            swap( values + max, values + i);
+            for (size_t j = 0; j < m.nRows; j++)
+                swap(&m.values[j] [max], &m.values[j][i]);
+        }
+    }
+}
+/*возвращает ’истина’, если матрица m
+является квадратной, ложь – в противном случае */
+bool isSquareMatrix(matrix *m) {
+    return m->nRows == m->nCols;
+}
+//возвращает ’истина’, если матрицы m1 и m2 равны, иначе - ложь
+bool areTwoMatricesEqual(matrix *m1, matrix *m2) {
+    if (m1->nCols != m2->nCols || m1->nRows != m2->nRows)
+        return 0;
+
+    for (size_t i = 0; i < m1->nRows; i++)
+        if (memcmp(m1->values[i], m2->values[i], sizeof(int) * m1->nCols))
+            return 0;
+
+    return 1;
+}
+//возвращает ’истина’, если матрица m является единичной, иначе - ложь
+bool isEMatrix(matrix *m) {
+//т.к Е матрица квадратная, то проверяем:
+    if (!isSquareMatrix(m))
+        return 0;
+
+    for (size_t i = 0; i < m->nRows; i++) {
+        if (m->values[i] [i] != 1)
+            return 0;
+
+        for (size_t j = 0; j < m->nCols; j++) {
+            if (i != j && m->values[i] [j] != 0)
+                return 0;
+        }
+    }
+
+    return 1;
+}
+//возвращает ’истина’, если матрица m является симметричной, иначе - ложь
+bool isSymmetricMatrix(matrix *m) {
+    if (!isSquareMatrix(m))
+        return 0;
+
+    for (size_t i = 0; i < m->nRows; i++) {
+        for ( size_t j = 0; j < m->nCols; j++) {
+            if ( m->values[i] [j] != m->values[j] [i])
+                return 0;
+        }
+    }
+
+    return 1;
+}
+
