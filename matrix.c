b518f50464e0a49c7b1f1c9ab8e48f3ccef98e74
Александр <alexanders.borchenko@gmail.com>
2024-03-13 12:03:03 +0300

    add ncols and nrows = 0

--- matrix.c
+++ matrix.c
@@ -33,6 +33,8 @@ void freeMemMatrix(matrix *m) {
     free(m->values);
 
     m->values = NULL;
+    m->nCols = 0;
+    m->nRows = 0;
 }
 //освобождает память, выделенную под хранение массива ms из nMatrices матриц.
 void freeMemMatrices(matrix *ms, int nMatrices) {
