cfa03f2a2ea3958dc88ba44d2a33f3c1dd16e3e6
Александр <alexanders.borchenko@gmail.com>
2024-03-21 17:09:20 +0300

    task 1 lab16 / test / bug fix

--- Lab 16 task.c
+++ Lab 16 task.c
@@ -3,27 +3,31 @@
 
 /*Дана квадратная матрица, все элементы которой различны. Поменять местами
 строки, в которых находятся максимальный и минимальный элементы */
-void task1(matrix *m) {
+void Lab16_task1(matrix *m) {
     int min = getMinValuePos(*m).rowIndex;
     int max = getMaxValuePos(*m).rowIndex;
     swapRows(m, max, min);
 }
-void test_task1() {
-    matrix m = createMatrixFromArray((int[]) {1, 1, 1,
-                                              2, 2, 2,
-                                              3, 3, 3},
+void test_Lab16_task1() {
+    matrix m = createMatrixFromArray((int[]) {
+        3, 3, 3,
+        4, 4, 4,
+        5, 5, 5},
                                      3, 3);
-    matrix m_test = createMatrixFromArray((int[]) {3, 3, 3,
-                                                   2, 2, 2,
-                                                   1, 1, 1},
+    matrix m_test = createMatrixFromArray((int[]) {
+        5, 5, 5,
+        4, 4, 4,
+        3, 3, 3},
                                           3, 3);
-    task1(&m);
+    Lab16_task1(&m);
+
     assert(areTwoMatricesEqual(&m, &m_test));
+
     freeMemMatrix(&m);
     freeMemMatrix(&m_test);
 }
 
 
 int main() {
-    test_task1();
+    test_Lab16_task1();
 }
